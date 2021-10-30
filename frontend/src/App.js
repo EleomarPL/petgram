ea9000e8e196044c4284fe34b5ef922b01e2b5fd
Eleomar pedro lorenzo <eleomarpedrolorenzo@hotmail.com>
2021-10-30 17:58:16 -0500

    frontend: added react-router-dom dependency and set app's routers

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,12 +5,34 @@ import '@fontsource/roboto';
 
 import './styles/index.css';
 
+import {BrowserRouter, Switch} from 'react-router-dom';
+
 import { AuthProvider } from './contexts/Auth';
+import PublicRoute from './components/router/PublicRouter';
+import PrivateRouter from './components/router/PrivateRouter';
 
 const App = () => {
   return (
     <AuthProvider>
-
+      <BrowserRouter>
+        <Switch>
+          <PublicRoute exact path="/">
+            <p>Inicio</p>
+          </PublicRoute>
+          <PublicRoute exact path="/register">
+            <p>Registrarse</p>
+          </PublicRoute>
+          <PublicRoute exact path="/login">
+            <p>Login</p>
+          </PublicRoute>
+          <PrivateRouter path="/home">
+            <p>Home</p>
+          </PrivateRouter>
+          <PrivateRouter exact path="/favorites">
+            <p>Favoritos</p>
+          </PrivateRouter>
+        </Switch>
+      </BrowserRouter>
     </AuthProvider>
   );
 };
