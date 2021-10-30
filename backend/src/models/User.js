5478e03dedaf1dda58006962337bf13ee14d9ead
Eleomar pedro lorenzo <eleomarpedrolorenzo@hotmail.com>
2021-10-30 14:50:44 -0500

    backend: model User was added

---
+++ backend/src/models/User.js
@@ -0,0 +1,49 @@
+const {Schema, model} = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    minlength: 2,
+    maxlength: 45
+  },
+  lastName: {
+    type: String,
+    minlength: 2,
+    maxlength: 45
+  },
+  motherLastName: {
+    type: String,
+    minlength: 2,
+    maxlength: 45
+  },
+  email: {
+    type: String,
+    minlength: 10,
+    maxlength: 80
+  },
+  userName: {
+    type: String,
+    minlength: 6,
+    maxlength: 45,
+    unique: true
+  },
+  password: String,
+  date: Date
+});
+
+userSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id;
+    delete returnedObject._id;
+    delete returnedObject.__v;
+
+    delete returnedObject.password;
+  }
+});
+
+userSchema.plugin(uniqueValidator);
+
+const User = model('User', userSchema);
+
+module.exports = User;
