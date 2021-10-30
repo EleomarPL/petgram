613bf455d2aba7d61f9681b3a0445c902253d5ff
Eleomar pedro lorenzo <eleomarpedrolorenzo@hotmail.com>
2021-10-30 13:19:02 -0500

    initial configuration app

---
+++ backend/src/index.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.set('port', process.env.PORT || 4000);
+
+app.listen(app.get('port'), () => {
+  console.log('Servidor en puerto ' + app.get('port'));
+  console.log(process.env.data);
+});
---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import '@fontsource/roboto';
+
+import './styles/index.css';
+
+const App = () => {
+  return (
+    <p>Hola mundo</p>
+  );
+};
+
+export default App;
