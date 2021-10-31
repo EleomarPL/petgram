f941cc11b995365da11e4ef2444398e0e2456ce5
Eleomar pedro lorenzo <eleomarpedrolorenzo@hotmail.com>
2021-10-31 11:11:32 -0600

    frontend: refactor create interaction service

--- frontend/src/services/api/interaction.js
+++ frontend/src/services/api/interaction.js
@@ -3,8 +3,15 @@ import { baseAPI } from '../../const/baseAPI';
 
 const BASE_API = `${baseAPI}/interaction`;
 
-export const createInteraction = async({ idPost, token }) => {
-  let response = await axios.post(`${BASE_API}/create-interaction/${idPost}'`, {}, token);
+export const createInteraction = async({idPost, token, photographerId, photographerUrl, photographer, url, srcImageSmall, srcImageMedium}) => {
+  let response = await axios.post(
+    `${BASE_API}/create-interaction/${idPost}'`,
+    {
+      photographerId, photographerUrl,
+      photographer, url, srcImageSmall, srcImageMedium
+    },
+    token
+  );
   return response;
 };
 export const getPosts = async({ searcher, token }) => {
