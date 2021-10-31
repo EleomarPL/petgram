4e6e16c9e0b78ec32964834b2149b1518aeb22de
Eleomar pedro lorenzo <eleomarpedrolorenzo@hotmail.com>
2021-10-30 19:15:33 -0500

    frontend: Spinners was added

---
+++ frontend/src/components/common/SpinnerButtonLoading.js
@@ -0,0 +1,11 @@
+const SpinnerButtonLoading = () => {
+  return (
+    <span
+      className="spinner-border spinner-border-sm" role="status"
+      aria-hidden="true"
+      style={ {marginRight: '0.5rem'} }
+    />
+  );
+};
+
+export default SpinnerButtonLoading;
