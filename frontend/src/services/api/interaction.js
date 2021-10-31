e6cf1841ff39c2b144fe3eb79b3813fa8f49f8cc
Eleomar pedro lorenzo <eleomarpedrolorenzo@hotmail.com>
2021-10-30 19:28:42 -0500

    frontend: repair API interaction

--- frontend/src/services/api/interaction.js
+++ frontend/src/services/api/interaction.js
@@ -2,10 +2,10 @@ import axios from 'axios';
 import { baseAPI } from '../../const/baseAPI';
 
 export const createInteraction = async({ idPost, token }) => {
-  let response = await axios.post(`${baseAPI}/create-interaction/${idPost}'`, {}, token);
+  let response = await axios.post(`${baseAPI}/interaction/create-interaction/${idPost}'`, {}, token);
   return response;
 };
 export const getPosts = async({ searcher, token }) => {
-  let response = await axios.get(`${baseAPI}/get-images/${searcher}'`, {}, token);
+  let response = await axios.get(`${baseAPI}/interaction/get-images/${searcher}'`, {}, token);
   return response;
 };
