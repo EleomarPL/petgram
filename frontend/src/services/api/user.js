3d721766d08830f239e1b69dc7748755ce3a4722
Eleomar pedro lorenzo <eleomarpedrolorenzo@hotmail.com>
2021-11-01 00:06:26 -0600

    repait package and api

--- frontend/src/services/api/user.js
+++ frontend/src/services/api/user.js
@@ -4,7 +4,7 @@ import { baseAPI } from '../../const/baseAPI';
 const BASE_API = `${baseAPI}/user`;
 
 export const createUser = async({ name, lastName, motherLastName, email, username, password }) => {
-  let response = await axios.post(`${BASE_API}}/create-user`,
+  let response = await axios.post(`${BASE_API}/create-user`,
     {
       name, lastName, motherLastName, email, username, password
     });
