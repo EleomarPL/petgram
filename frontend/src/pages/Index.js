25bbfd2bcec56604cc35fa8dc0f293d396e8eb78
Eleomar Pedro Lorenzo <eleomarpedrolorenzo@gmail.com>
2022-12-18 10:59:57 -0600

    fix: Page home was fixed with nextui

--- frontend/src/pages/Index.js
+++ frontend/src/pages/Index.js
@@ -1,42 +1,61 @@
+import { Avatar, Container, Grid, Image, Link, Text } from '@nextui-org/react';
 import { contactLinks } from '../data/contactLinks';
 
 const Index = () => {
   return (
     <section>
-      <h1 className="text-center">PetGram</h1>
+      <Text
+        h1
+        size={ 60 }
+        css={ {
+          textGradient: '45deg, $blue600 -20%, $pink600 50%',
+          textAlign: 'center'
+        } }
+        weight="bold"
+      >
+        Petgram
+      </Text>
       <picture>
         <source srcSet={ require('../img/index.webp').default } />
-        <div className="d-flex justify-content-center">
-          <img src={ require('../img/index.webp').default }
-            style={ {
-              width: '15rem',
-              objectFit: 'cover'
-            } }
-          />
-        </div>
-        <div className="text-center">
-          <p>
-            Aplicación web que simula Instagram, pero con operaciones básicas,
-            que son crear usuarios y darle me gusta a post predefinidos.
-          </p>
-          <p>Creador: Eleomar Pedro Lorenzo</p>
-          <p>Contactame</p>
-          <div className="d-flex flex-wrap justify-content-center">
-            { contactLinks &&
-                contactLinks.map( contact =>
-                  <a href={ contact.link }
-                    key={ contact.className }
-                    style={ {fontSize: '2rem', color: 'black', marginRight: '1rem'} }
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <i className={ contact.className }></i>
-                  </a>
-                )
-            }
-          </div>
-        </div>
+        <Image
+          width="15rem"
+          height={ 180 }
+          src={ require('../img/index.webp').default }
+          alt="Development people"
+          objectFit="cover"
+        />
       </picture>
+      <Container css={ {
+        textAlign: 'center'
+      } }>
+        <Text css={ { fontFamily: 'inherit' } }>
+          Aplicación web que simula Instagram, pero con operaciones básicas,
+          que son crear usuarios y darle me gusta a post predefinidos.
+        </Text>
+        <Text>Creador: Eleomar Pedro Lorenzo</Text>
+        <Text>Contactame</Text>
+        <Grid.Container gap={ 2 } css={ { placeContent: 'center' } }>
+          { contactLinks &&
+                contactLinks.map( contact =>
+                  <Grid key={ contact.className }>
+                    <Avatar squared
+                      icon={ <i style={ { fontSize: '1.8rem' } } className={ contact.className } /> }
+                      as={ Link }
+                      target="_blank"
+                      rel="noreferrer"
+                      href={ contact.link }
+                      css={ {
+                        color: '#000',
+                        cursor: 'pointer'
+                      } }
+                    />
+                  </Grid>
+                  
+                )
+          }
+        </Grid.Container>
+      </Container>
+      
     </section>
   );
 };
